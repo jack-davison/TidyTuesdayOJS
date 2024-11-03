61b9d5e30dde10dc39ee21df3fd71e33fa015a76
Jack Davison <45171616+jack-davison@users.noreply.github.com>
2024-11-03 13:01:11 +0000

    feat: simplify reading script
    and remove duplicate actions

--- src/data/monster_movies.json.R
+++ src/data/monster_movies.json.R
@@ -1,7 +1,6 @@
 
-monsters <- tidytuesdayR::tt_load(2024, 44)
 
-movies <- monsters$monster_movies
+movies <- read.csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/refs/heads/master/data/2024/2024-10-29/monster_movies.csv")
 
 movies$genres[is.na(movies$genres)] <- "Unknown"
  
